b8b735bea37d52168d3e50120cf4f8039e532b8d
Sid <siddharth@crowdanalytix.com>
2018-06-26 22:06:49 +0530

    Adding Up New Fav Icon + Clean Up Code.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
 console.log("Hello Sid!")
+    function menuColapse() {
+        var x = document.getElementById("menu");
+        if (x.className.indexOf("w3-show") == -1) {
+            x.className += " w3-show";
+        } else { 
+            x.className = x.className.replace(" w3-show", "");
+        }
+    }
