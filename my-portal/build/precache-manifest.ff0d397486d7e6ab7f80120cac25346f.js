beb97d56003e00f66a1855535cbdbe512bfec77d
“im-siddharth” <siddharth@getmyparking.com>
2020-08-08 21:36:24 +0530

    fix

---
+++ my-portal/build/precache-manifest.ff0d397486d7e6ab7f80120cac25346f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58ffd209170b6baf6408d8e4ddc9eaab",
+    "url": "/siddz05.github.io/my-portal/index.html"
+  },
+  {
+    "revision": "f665aaeebb976c837930",
+    "url": "/siddz05.github.io/my-portal/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "277840b38e64e9b3ff70",
+    "url": "/siddz05.github.io/my-portal/static/js/2.81874adf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/siddz05.github.io/my-portal/static/js/2.81874adf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f665aaeebb976c837930",
+    "url": "/siddz05.github.io/my-portal/static/js/main.0fb8a0a3.chunk.js"
+  },
+  {
+    "revision": "519d38bdd89b52887b40",
+    "url": "/siddz05.github.io/my-portal/static/js/runtime-main.f5c402ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/siddz05.github.io/my-portal/static/media/logo.5d5d9eef.svg"
+  }
+]);
